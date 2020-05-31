29550783851f4e1c34591f2d1b85d312ef9bb157
Vladimir Cvetanovic <cvetanovic9696@gmail.com>
2020-05-31 19:23:01 +0200

    Najvaznija obelezja random foresta

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import pydot as pydot
 import requests
 from bs4 import BeautifulSoup
 import os
 import csv
 import numpy as np
 from chefboost import Chefboost as chef
+from six import StringIO
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,12 +15,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import RandomizedSearchCV
 from pprint import pprint
-
+from sklearn.model_selection import GridSearchCV
 # Scatterplot Matrix
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 from pandas.plotting import scatter_matrix
-
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.tree import export_graphviz
 
 # id wrappera = voting_results_DIVIZIJA
 # svaka tabela ima id formata: voting-results-DIVIZIJA_IDPOZICIJE
@@ -139,8 +143,6 @@ def write_file_sorted(path, filename, rows):
     for row in rows:
         all_votes_sum = all_votes_sum + row[2]
 
-
-
     directory = os.path.realpath(path);
     foupath = os.path.join(directory, filename + ".csv");
     with open(foupath, 'w', encoding="utf-8", newline='') as file:
@@ -148,7 +150,7 @@ def write_file_sorted(path, filename, rows):
         w.writerow(['name', 'votes', 'all_star_share'])
         for row in rows:
             w.writerow([row[1], row[2], calculate_percentage(all_votes_sum, row[2])])
-
+    return all_votes_sum
 
 def write_file_2017(path, filename, rows):
     directory = os.path.realpath(path);
@@ -194,14 +196,17 @@ def scrape_season_2017_2020():
 
 def sort_all_star_votes():
     counter = 1979
+    votes_per_season = []
     for index in range(1980, 2013):
         counter = counter + 1
         if(counter == 1999):
             continue
-        write_and_sort(str(index))
+        votes_per_season.append([index, write_and_sort(str(index))])
 
     for index in range(2013, 2020):
-        write_and_sort_courts(str(index))
+        votes_per_season.append([index, write_and_sort_courts(str(index))])
+
+    return votes_per_season
 
 
 def read_csv(path, votes):
@@ -250,7 +255,8 @@ def write_and_sort(year):
         r"C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping\all-star-votings-" + year + r"\table_forward_eastern.csv",
         votes)
     votes_sorted = sorted(votes, key=lambda vote: vote[2], reverse=True)
-    write_file_sorted(r"C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping\all-star-votings-" + year, "sorted_votes_" + year, votes_sorted)
+    sum = write_file_sorted(r"C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping\all-star-votings-" + year, "sorted_votes_" + year, votes_sorted)
+    return sum
 
 def write_and_sort_courts(year):
     votes = []
@@ -269,8 +275,9 @@ def write_and_sort_courts(year):
     print(votes)
     votes_sorted = sorted(votes, key=lambda vote: vote[2], reverse=True)
     print(votes_sorted)
-    write_file_sorted(r"C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping\all-star-votings-" + year,
+    sum = write_file_sorted(r"C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping\all-star-votings-" + year,
                       "sorted_votes_" + year, votes_sorted)
+    return sum
 
 
 def merge_files(year, votes):
@@ -467,37 +474,76 @@ def random_forest_regression(X, y, year):
     test_features = np.array(y.drop('award_share', axis=1))
     test_labels = np.array(y['award_share'])
 
-
-    r1 = RandomForestRegressor(n_estimators=1000, random_state=0, bootstrap=True)
-    r2 = RandomForestRegressor(n_estimators=500, random_state=0, bootstrap=True)
+    r1 = RandomForestRegressor(n_estimators=1500, random_state=42, bootstrap=True, max_depth=80,
+                               max_features='sqrt', min_samples_leaf=5, min_samples_split=3)
     r1.fit(train_features, train_labels)
-    r2.fit(train_features, train_labels)
-
     predictions_r1 = r1.predict(test_features)
-    predictions_r2 = r2.predict(test_features)
 
-    data = pd.read_csv('test_' + str(year) + '.csv', usecols=['player', 'season', 'award_share', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm', 'all_star_share'])
+    evaluate(r1, test_features, test_labels)
+
+    data = pd.read_csv('test_' + str(year) + '.csv', usecols=['player', 'season', 'award_share',  'pts_per_g','per', 'ws', 'bpm', 'all_star_share'])
     players = np.array(data['player'])
     seasons = np.array(data['season'])
     award_shares = np.array(data['award_share'])
-    votes_first = np.array(data['votes_first'])
     per = np.array(data['per'])
     ws = np.array(data['ws'])
-    ws_per_48 = np.array(data['ws_per_48'])
+    pts_per_g = np.array(data['pts_per_g'])
     bpm = np.array(data['bpm'])
     all_star_share = np.array(data['all_star_share'])
-    tuples = list(zip(players, seasons, award_shares, predictions_r1, votes_first, per, ws, ws_per_48, bpm, all_star_share))
-    df = pd.DataFrame(tuples, columns=['player', 'season', 'award_share', 'predictions', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm', 'all_star_share'])
+    tuples = list(zip(players, seasons, award_shares, predictions_r1, per, ws, bpm, pts_per_g, all_star_share))
+    df = pd.DataFrame(tuples, columns=['player', 'season', 'award_share', 'predictions', 'per', 'ws', 'bpm', 'pts_per_g', 'all_star_share'])
 
-    print(df[0:6])
-    print("\n")
+    return df
 
-    # Print out the mean absolute error (mae)
-    print('Mean Absolute Error r1:', round(mean_absolute_error(test_labels, predictions_r1),2), '.')
-    print('Mean Absolute Error r2:', round(mean_absolute_error(test_labels, predictions_r2),2), '.')
 
-    print("Score r1: " + str(r1.score(test_features, test_labels)))
-    print("Score r2: " + str(r2.score(test_features, test_labels)))
+def make_random_forest_regressor(is_all_star, X):
+    target = np.array(X['award_share'])
+    features = X.drop('award_share', axis=1)
+    feature_list = list(features.columns)
+    features = np.array(features)
+
+    train_features = features;
+    train_labels = target;
+
+    r = RandomForestRegressor()
+    if is_all_star == True:
+        r = RandomForestRegressor(n_estimators=1000, random_state=42, bootstrap=True, max_depth=40,
+                                   max_features='sqrt', min_samples_leaf=5, min_samples_split=10)
+        r.fit(train_features, train_labels)
+    else:
+        r = RandomForestRegressor(n_estimators=1500, random_state=42, bootstrap=True, max_depth=80,
+                                   max_features='sqrt', min_samples_leaf=5, min_samples_split=3)
+        r.fit(train_features, train_labels)
+
+    return r;
+
+def predict(regressor, X, y, year):
+
+    target = np.array(X['award_share'])
+    features = X.drop('award_share', axis=1)
+    feature_list = list(features.columns)
+    features = np.array(features)
+
+    train_features = features;
+    train_labels = target;
+
+    test_features = np.array(y.drop('award_share', axis=1))
+    test_labels = np.array(y['award_share'])
+    predictions_r1 = regressor.predict(test_features)
+
+    evaluate(regressor, test_features, test_labels)
+
+    data = pd.read_csv('test_' + str(year) + '.csv', usecols=['player', 'season', 'award_share', 'pts_per_g','per', 'ws', 'bpm', 'all_star_share'])
+    players = np.array(data['player'])
+    seasons = np.array(data['season'])
+    award_shares = np.array(data['award_share'])
+    per = np.array(data['per'])
+    ws = np.array(data['ws'])
+    pts_per_g = np.array(data['pts_per_g'])
+    bpm = np.array(data['bpm'])
+    all_star_share = np.array(data['all_star_share'])
+    tuples = list(zip(players, seasons, award_shares, predictions_r1, per, ws, bpm, pts_per_g, all_star_share))
+    df = pd.DataFrame(tuples, columns=['player', 'season', 'award_share', 'predictions', 'per', 'ws', 'bpm', 'pts_per_g', 'all_star_share'])
 
     return df
 
@@ -510,6 +556,8 @@ def check_results(dataframe):
     # pd.plotting.table(ax,real)
     # pd.plotting.table(ax,prediction)
     # plt.show()
+    pd.set_option('display.width', 400)
+    pd.set_option('display.max_columns', 10)
     print('\n')
     print(real[0:6])
     print('\n')
@@ -517,7 +565,15 @@ def check_results(dataframe):
     print('\n')
     return real[0:6], prediction[0:6]
 
-def random_forest_tuning(X,y):
+
+def evaluate(model, test_features, test_labels):
+    predictions = model.predict(test_features)
+    errors = abs(predictions - test_labels)
+    print('Model Performance')
+    print('Mean absolute error: {:0.4f} degrees.'.format(mean_absolute_error(test_labels, predictions)))
+    print('R2 score: ' + str(r2_score(test_labels, predictions)))
+
+def random_search(X,y):
     # get train and test features and labels
     target = np.array(X['award_share'])
     features = X.drop('award_share', axis=1)
@@ -551,12 +607,13 @@ def random_forest_tuning(X,y):
                    'bootstrap': bootstrap}
     pprint(random_grid)
 
+    # RANDOM SEARCH
     # Use the random grid to search for best hyperparameters
     # First create the base model to tune
     rf = RandomForestRegressor()
     # Random search of parameters, using 3 fold cross validation,
     # search across 100 different combinations, and use all available cores
-    rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=100, cv=3, verbose=2,
+    rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=100, cv=5, verbose=2,
                                    random_state=42, n_jobs=-1)
     # Fit the random search model
     rf_random.fit(train_features, train_labels)
@@ -572,17 +629,62 @@ def random_forest_tuning(X,y):
     best_random = rf_random.best_estimator_
     best_random_predictions = best_random.predict(test_features)
 
+    errors_base_model = abs(base_model_predictions - test_labels)
+    errors_best_random_model = abs(best_random_predictions - test_labels)
+
+    print("************** Evaluacija za best_random i base_model **************")
+    evaluate(best_random, test_features, test_labels)
+    evaluate(base_model, test_features, test_labels)
     # Print out the mean absolute error (mae)
-    print('Mean Absolute Error r1:', round(mean_absolute_error(test_labels, base_model_predictions), 5), '.')
-    print('Mean Absolute Error r1:', round(mean_absolute_error(test_labels, best_random_predictions), 5), '.')
+    print('Mean Absolute Error base:', round(mean_absolute_error(test_labels, base_model_predictions), 5), '.')
+    print('Mean Absolute Error random:', round(mean_absolute_error(test_labels, best_random_predictions), 5), '.')
+
+def grid_search(X, y):
+    # get train and test features and labels
+    target = np.array(X['award_share'])
+    features = X.drop('award_share', axis=1)
+    feature_list = list(features.columns)
+    features = np.array(features)
+
+    train_features = features;
+    train_labels = target;
+    test_features = np.array(y.drop('award_share', axis=1))
+    test_labels = np.array(y['award_share'])
+
+    # GRID SEARCH
+    # Create the parameter grid based on the results of random search
+    param_grid = {
+        'bootstrap': [True],
+        'max_depth': [80, 90, 100, 110],
+        'max_features': ['sqrt'],
+        'min_samples_leaf': [3, 4, 5],
+        'min_samples_split': [2, 3, 4],
+        'n_estimators': [1500, 1800, 2000, 2200]
+    }
+
+    # Create a based model
+    rf = RandomForestRegressor()
+    # Instantiate the grid search model
+    grid_search = GridSearchCV(estimator = rf, param_grid = param_grid,
+                              cv = 5, n_jobs = -1, verbose = 2)
+    grid_search.fit(train_features, train_labels)
+
+    print("************** Evaluacija za grid_search **************")
+    grid_search.best_params_
+    pprint(grid_search.best_params_)
+    best_grid = grid_search.best_estimator_
+    evaluate(best_grid, test_features, test_labels)
 
 
 if __name__ == '__main__':
     # scrape_season_1980_2012()
     # scrape_season_2013_2016()
     # scrape_season_2017_2020()
-    #
-    # sort_all_star_votes()
+    # votes_per_season = []
+    # # ovde mogu dobiti za svaku god ukupan broj glasova
+    # votes_per_season = sort_all_star_votes()
+    # data_frame_votes_per_season = pd.DataFrame(votes_per_season,columns=['season', 'votes'])
+    # data_frame_votes_per_season.to_csv('votes_per_season.csv', sep='\t', encoding='utf-8')
     # merge_all()
     #
     # all_star_history_votes = make_dataset(r'C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping\all_star_votes_history')
@@ -595,54 +697,106 @@ if __name__ == '__main__':
     # test_data = make_dataset(r'C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping\test_data.csv')
     # dataset_test_data = join_datasets_test_data(all_star_votes_2019, test_data)
     # write_joined_csv_test_data(dataset_test_data, r'C:\Users\Vladimir\PycharmProjects\all-star-votings-scrapping', 'test_data_joined')
+    #
 
-
-    # # PEARSON CORRELATION
+    # PEARSON CORRELATION
+    X = pd.read_csv('mvp_votings_joined.csv', usecols=['award_share', 'pts_per_g', 'per', 'ws', 'ws_per_48', 'bpm', 'all_star_share'])
     # X = pd.read_csv('mvp_votings_joined.csv')
-    # corr = X.corr()
-    # coef = corr['award_share'].sort_values(ascending=False)
-    # coef.to_csv('person_correlations.csv', sep='\t', encoding='utf-8')
+    corr = X.corr()
+    coef = corr['award_share'].sort_values(ascending=False)
+    corr.to_csv('person_correlations.csv', sep='\t', encoding='utf-8')
+
+    # plt.scatter(X['per'], X['award_share'])
+    # plt.xlabel('per')
+    # plt.ylabel('award_share')
+    # plt.show()
     # plt.scatter(X['ws'], X['award_share'])
     # plt.xlabel('ws')
     # plt.ylabel('award_share')
     # plt.show()
+    # TREND GLASOVA KROZ GODINE
+    # votes_per_season = pd.read_csv('votes_per_season.csv', delimiter='\t')
+    # plt.plot(votes_per_season['season'], votes_per_season['votes'])
+    # plt.xlabel('season')
+    # plt.ylabel('votes')
+    # plt.show()
+    # plt.scatter(X['all_star_share'], X['award_share'])
+    # plt.xlabel('all_star_share')
+    # plt.ylabel('award_share')
+    # plt.show()
+
 
     # RANDOM FOREST
-    # X = pd.read_csv('train.csv', usecols=['award_share', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm'])
-    # y = pd.read_csv('test_2013.csv', usecols=['award_share', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm'])
-    # random_forest_tuning(X,y)
+    X = pd.read_csv('train.csv', usecols=['award_share', 'pts_per_g', 'per', 'ws', 'bpm'])
+    y = pd.read_csv('test_2013.csv', usecols=['award_share', 'pts_per_g', 'per', 'ws', 'bpm'])
+    X_all_star = pd.read_csv('train.csv', usecols=['award_share', 'pts_per_g', 'per', 'ws', 'bpm', 'all_star_share'])
+    y_all_star = pd.read_csv('test_2013.csv', usecols=['award_share', 'pts_per_g', 'per', 'ws', 'bpm', 'all_star_share'])
+    # random_search(X,y)
+    # random_search(X_all_star,y_all_star)
+    # grid_search(X, y)
+    # grid_search(X_all_star, y_all_star)
+    regressor_all_star = make_random_forest_regressor(True, X_all_star)
+    regressor = make_random_forest_regressor(False, X)
 
-    X_all_star = pd.read_csv('train.csv', usecols=['award_share', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm', 'all_star_share'])
-    X = pd.read_csv('train.csv', usecols=['award_share', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm'])
     for index in range(2013,2018):
         print('****************** ALL_STAR_SHARE ******************  \n')
         print("Season:" + str(index))
-        y_all_star = pd.read_csv('test_' + str(index) + '.csv', usecols=['award_share', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm', 'all_star_share'])
-        df_all_star = random_forest_regression(X_all_star, y_all_star, index)
+        y_all_star = pd.read_csv('test_' + str(index) + '.csv', usecols=['award_share', 'pts_per_g', 'per', 'ws', 'bpm', 'all_star_share'])
+        df_all_star = predict(regressor_all_star,X_all_star, y_all_star, index)
         real_all_star, predicition_all_star = check_results(df_all_star)
         print('\n')
         print('****************** ALL_STAR_SHARE ******************  \n\n\n')
 
-        print('****************** NO_ALL_STAR_SHARE ******************  \n')
+        print('****************** NO_ALL_STAR_SHARE ******************  ')
         print("Season:" + str(index))
-        y = pd.read_csv('test_' + str(index) +  '.csv', usecols=['award_share', 'votes_first', 'per', 'ws', 'ws_per_48', 'bpm'])
-        df = random_forest_regression(X, y, index)
+        y = pd.read_csv('test_' + str(index) +  '.csv', usecols=['award_share', 'pts_per_g', 'per', 'ws', 'bpm'])
+        df = predict(regressor, X, y, index)
         real, prediction = check_results(df)
         print('\n')
+        print('****************** NO_ALL_STAR_SHARE ******************  \n')
 
-        with open('predictions_all_star' + str(index) + '.csv', 'w') as f:
+        with open('predictions_all_star_' + str(index) + '.csv', 'w') as f:
             real_all_star.to_csv(f)
-        with open('predictions_all_star' + str(index) + '.csv', 'a') as f:
+        with open('predictions_all_star_' + str(index) + '.csv', 'a') as f:
             predicition_all_star.to_csv(f)
 
         with open('predictions_' + str(index) + '.csv', 'w') as f:
             real.to_csv(f)
         with open('predictions_' + str(index) + '.csv', 'a') as f:
             prediction.to_csv(f)
-        # pd.concat([real_all_star, predicition_all_star], axis=1).to_csv('predictions_all_star_' + str(index) + '.csv')
-        # pd.concat([real, prediction], axis=1).to_csv('predictions_' + str(index) + '.csv')
 
+    features = X.drop('award_share', axis=1)
+    feature_list = list(features.columns)
+    importances = regressor.feature_importances_
+    indices = np.argsort(importances)
+    plt.title('Feature Importances')
+    plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+    plt.yticks(range(len(indices)), [feature_list[i] for i in indices])
+    plt.xlabel('Relative Importance')
+    plt.show()
 
+    features_all_star = X_all_star.drop('award_share', axis=1)
+    feature_list_all_star = list(features_all_star.columns)
+    importances_all_star = regressor_all_star.feature_importances_
+    indices_all_star = np.argsort(importances_all_star)
+    plt.title('Feature Importances All star')
+    plt.barh(range(len(indices_all_star)), importances_all_star[indices_all_star], color='b', align='center')
+    plt.yticks(range(len(indices_all_star)), [feature_list_all_star[i] for i in indices_all_star])
+    plt.xlabel('Relative Importance')
+    plt.show()
+
+    tree = regressor.estimators_
+    estimator_one = regressor.estimators_[0]
+    estimator_full = regressor.estimators_
+    # Export as dot file
+    export_graphviz(estimator_one, out_file='tree_one.dot',
+                    feature_names=features.columns,
+                    rounded=True, proportion=False,
+                    precision=2, filled=True)
+    export_graphviz(estimator_full, out_file='tree_full.dot',
+                    feature_names=features.columns,
+                    rounded=True, proportion=False,
+                    precision=2, filled=True)
 
     # #LINEAR
     # linear_regression()
@@ -652,5 +806,4 @@ if __name__ == '__main__':
     #     df = gradient_boosting(index)
     #     real, prediction = check_results(df)
     #     pd.concat([real, prediction], axis=1).to_csv('predictions_' + str(index)+"_gb" + '.csv')
-    #
 
